62c72cc942a4abf31edbe4c029b6d85ee5d051be
Asia Ashraf <asiaashraf7272@gmail.com>
2026-03-25 12:59:27 +0500

    working on adminfeedback table

--- components/admin-feedback-table.tsx
+++ components/admin-feedback-table.tsx
@@ -3,5 +3,6 @@ import { useState } from "react"
 
 export default function AdminFeedbackTable({ posts }: { posts: any[] }) {
     const [editingPostId, setEditingPostId] = useState<string | null>(null);
+    const [postStatus, setPostStatus] = useState<Record<string, string>>();
     return <>Admin feedback table</>
 }
