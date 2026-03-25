cd649d052ff139ebc9d880d6058210a16cc28bab
Asia Ashraf <asiaashraf7272@gmail.com>
2026-03-25 20:04:30 +0500

    working on admin page

--- components/admin-feedback-table.tsx
+++ components/admin-feedback-table.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Table, TableHead, TableHeader, TableRow } from "./ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+import { getCategoryDesign } from "@/app/data/category-data";
+import { Badge } from "./ui/badge";
 
 export default function AdminFeedbackTable({ posts }: { posts: any[] }) {
     const [editingPostId, setEditingPostId] = useState<string | null>(null);
@@ -25,6 +27,24 @@ export default function AdminFeedbackTable({ posts }: { posts: any[] }) {
                             <TableHead>Action</TableHead>
                         </TableRow>
                     </TableHeader>
+                    <TableBody>
+                        {posts.map((post) => {
+                            const categoryDesign = getCategoryDesign(post.category)
+                            const CategoryIcon = categoryDesign.icon;
+                            return (
+                                <TableRow key={post.id} className="h-17.5">
+                                    <TableCell className="font-medium max-w-xs truncate align-middle">
+                                        {post.title}
+                                    </TableCell>
+                                    <TableCell className="align-middle">
+                                        <Badge variant="outline" className={`${categoryDesign.text} ${categoryDesign.border}`}>
+                                            {post.category}
+                                        </Badge>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
                 </Table>
             </CardContent>
         </Card>
