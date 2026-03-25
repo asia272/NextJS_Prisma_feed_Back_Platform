9d92c0cf5e092032fec7371c9d198780117326a1
Asia Ashraf <asiaashraf7272@gmail.com>
2026-03-25 14:03:28 +0500

    working on admin page

--- components/admin-feedback-table.tsx
+++ components/admin-feedback-table.tsx
@@ -1,8 +1,32 @@
 "use client";
 import { useState } from "react"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Table, TableHead, TableHeader, TableRow } from "./ui/table";
 
 export default function AdminFeedbackTable({ posts }: { posts: any[] }) {
     const [editingPostId, setEditingPostId] = useState<string | null>(null);
-    const [postStatus, setPostStatus] = useState<Record<string, string>>();
-    return <>Admin feedback table</>
+    const [postStatus, setPostStatus] = useState<Record<string, string>>(
+        Object.fromEntries(posts.map((post) => [post.id, post.status]))
+    );
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Manage Feedback</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Title</TableHead>
+                            <TableHead>Category</TableHead>
+                            <TableHead>Votes</TableHead>
+                            <TableHead>Author</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                </Table>
+            </CardContent>
+        </Card>
+    )
 }
