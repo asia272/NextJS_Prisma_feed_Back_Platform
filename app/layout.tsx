6eecf02ceadc012b322a48883d9f2bfef11e11d5
asia272 <asiaashraf7272@gmail.com>
2026-04-21 13:51:48 +0500

    fix header color

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ export const dynamic = "force-dynamic";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
+import "./globals.css"
+
 import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { syncCurrentUser } from "@/lib/sync-user";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
