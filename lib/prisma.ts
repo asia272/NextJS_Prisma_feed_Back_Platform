876e040ad74528c14e4830480db645ca853def5f
asia272 <asiaashraf7272@gmail.com>
2026-04-22 09:08:29 +0500

    fix neon db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,39 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+// /* eslint-disable no-var */
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const prisma =
+//   global.prisma ||
+//   new PrismaClient({
+//     log: ["error", "warn"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   global.prisma = prisma;
+// }
+// export default prisma;
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const prisma =
-  globalForPrisma.prisma ||
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["error", "warn"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
 export default prisma;
